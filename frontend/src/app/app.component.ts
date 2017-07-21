f563efe0101ac152d48c853e2496aefbda21cfe5
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-07-20 21:25:52 -0500

    Implemented forms for login and register

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Musify Frontend!';
+  public title = 'Musify!';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+    this.user = new User("","","","","","ROLE_USER","");
+  }
 }
