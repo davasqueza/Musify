a5ffa7c74910fb57720c3173acad96ad7213650a
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-08-07 14:07:55 -0500

    Implemented cycle for upload user image

--- frontend/src/app/components/user-edit.component.ts
+++ frontend/src/app/components/user-edit.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit} from "@angular/core"
 import { AppService } from "../app.service"
 import { UserService } from "../services/user.service"
 import { User } from "../models/user"
+import { GLOBAL } from "../services/global"
 
 @Component({
   selector: "user-edit",
@@ -16,6 +17,8 @@ export class UserEditComponent implements OnInit{
   public identity;
   public token;
   public updateFormMessage;
+  public filesToUpload: Array<File>;
+  public url: string;
 
   constructor(private _userService:UserService, private _appService:AppService){
     this.titulo = "Actualizar mis datos";
@@ -23,6 +26,7 @@ export class UserEditComponent implements OnInit{
     this.token = this._userService.getToken();
 
     this.user = this.identity;
+    this.url = GLOBAL.url;
   }
 
   ngOnInit(){
@@ -33,6 +37,17 @@ export class UserEditComponent implements OnInit{
     let success = function () {
       localStorage.setItem("identity", JSON.stringify(this.user));
       this._appService.emitReloadUser();
+
+      if(!this.filesToUpload){
+
+      }
+      else{
+        this.makeFileRequest(this.url+"uploadUserImage/"+this.user._id, [], this.filesToUpload)
+          .then(function (result) {
+            this.user.image = result.image;
+            localStorage.setItem("identity", JSON.stringify(this.user));
+          }.bind(this));
+      }
 
       this.updateFormMessage = "El usuario se ha actualizado correctamente";
     };
@@ -46,4 +61,32 @@ export class UserEditComponent implements OnInit{
     this._userService.update(this.user).subscribe(success.bind(this), error.bind(this));
   }
 
+  fileChangeEvent(fileInput){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
+
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+    let token = this.token;
+    return new Promise(function (resolve, reject) {
+      let formData = new FormData();
+      let xhr = new XMLHttpRequest();
+
+      for(let i = 0; i<files.length; i++){
+        formData.append("image", files[i], files[i].name);
+        xhr.onreadystatechange = function () {
+          if(xhr.readyState === 4){
+            if(xhr.status === 200){
+              resolve(JSON.parse(xhr.response));
+            }
+            else {
+              reject(xhr.response);
+            }
+          }
+        };
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Authorization", token);
+        xhr.send(formData);
+      }
+    });
+  }
 }
